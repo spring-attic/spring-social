858f60c59a4ca3381a410b861ba0690eb8a35a42
Craig Walls <cwalls@vmware.com>
2010-10-27 22:03:10 -0500

    Gradle-ified Spring Social build

--- spring-social-core/src/main/java/org/springframework/social/oauth1/MissingOAuthLibraryException.java
+++ spring-social-core/src/main/java/org/springframework/social/oauth1/MissingOAuthLibraryException.java
@@ -3,6 +3,8 @@ package org.springframework.social.oauth1;
 import org.springframework.social.core.SocialException;
 
 public class MissingOAuthLibraryException extends SocialException {
+    private static final long serialVersionUID = 1L;
+	
 	public MissingOAuthLibraryException(String message) {
 		super(message);
 	}
