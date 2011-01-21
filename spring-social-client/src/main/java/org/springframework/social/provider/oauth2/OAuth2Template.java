345bb123b4a06ce0678db1de6c2a81895843df8d
Craig Walls <cwalls@vmware.com>
2011-01-21 10:35:29 -0600

    Added implementation details to OAuth2Template

--- spring-social-client/src/main/java/org/springframework/social/provider/oauth2/OAuth2Template.java
+++ spring-social-client/src/main/java/org/springframework/social/provider/oauth2/OAuth2Template.java
@@ -15,6 +15,13 @@
  */
 package org.springframework.social.provider.oauth2;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriTemplate;
+
 /**
  * OAuth2Operations implementation that uses REST-template to make the OAuth calls.
  * @author Keith Donald
@@ -24,18 +31,39 @@ public class OAuth2Template implements OAuth2Operations {
 	private final String clientId;
 	
 	private final String clientSecret;
+
+	private final String accessTokenUrl;
+
+	private final UriTemplate authorizeUrlTemplate;
 	
-	public OAuth2Template(String clientId, String clientSecret) {
+	public OAuth2Template(String clientId, String clientSecret, String authorizeUrl, String accessTokenUrl) {
 		this.clientId = clientId;
 		this.clientSecret = clientSecret;
+		this.authorizeUrlTemplate = authorizeUrl != null ? new UriTemplate(authorizeUrl) : null;
+		this.accessTokenUrl = accessTokenUrl;
 	}
 
 	public String buildAuthorizeUrl(String redirectUri, String scope) {
-		return null;
+		Map<String, String> urlVariables = new HashMap<String, String>();
+		urlVariables.put("clientId", clientId);
+		urlVariables.put("redirectUri", redirectUri);
+		urlVariables.put("scope", scope);
+		return authorizeUrlTemplate.expand(urlVariables).toString();
 	}
 
 	public AccessToken exchangeForAccessToken(String redirectUri, String authorizationCode) {
-		return null;
+		Map<String, String> requestParameters = new HashMap<String, String>();
+		requestParameters.put("client_id", clientId);
+		requestParameters.put("client_secret", clientSecret);
+		requestParameters.put("code", authorizationCode);
+		requestParameters.put("redirect_uri", redirectUri);
+		requestParameters.put("grant_type", "authorization_code");
+		@SuppressWarnings("unchecked")
+		Map<String, String> result = getRestOperations().postForObject(accessTokenUrl, requestParameters, Map.class);
+		return new AccessToken(result.get("access_token"), result.get("refresh_token"));
 	}
-	
+
+	protected RestOperations getRestOperations() {
+		return new RestTemplate();
+	}
 }
