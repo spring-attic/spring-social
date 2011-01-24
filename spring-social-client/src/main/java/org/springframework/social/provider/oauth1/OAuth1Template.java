82367ef43dfc83e84b6ccf0e1244b55eaa6bf05a
Craig Walls <cwalls@vmware.com>
2011-01-24 10:01:10 -0600

    Changed OAuth1Template to contain a standalone implementation of OAuth 1 (ie, no dependency on a 3rd party OAuth library of any kind). Note that this only addresses the happy path at this point.

--- spring-social-client/src/main/java/org/springframework/social/provider/oauth1/OAuth1Template.java
+++ spring-social-client/src/main/java/org/springframework/social/provider/oauth1/OAuth1Template.java
@@ -15,12 +15,27 @@
  */
 package org.springframework.social.provider.oauth1;
 
-import org.springframework.security.oauth.common.signature.SharedConsumerSecret;
-import org.springframework.security.oauth.consumer.BaseProtectedResourceDetails;
-import org.springframework.security.oauth.consumer.CoreOAuthConsumerSupport;
-import org.springframework.security.oauth.consumer.OAuthConsumerSupport;
-import org.springframework.security.oauth.consumer.ProtectedResourceDetails;
-import org.springframework.security.oauth.consumer.token.OAuthConsumerToken;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 
 /**
@@ -38,8 +53,6 @@ public class OAuth1Template implements OAuth1Operations {
 	private final UriTemplate authorizeUrlTemplate;
 	
 	private final String accessTokenUrl;
-
-	private OAuthConsumerSupport oauthConsumerSupport;
 	
 	public OAuth1Template(String consumerKey, String consumerSecret, String requestTokenUrl, String authorizeUrl, String accessTokenUrl) {
 		this.consumerKey = consumerKey;
@@ -47,13 +60,12 @@ public class OAuth1Template implements OAuth1Operations {
 		this.requestTokenUrl = requestTokenUrl;
 		this.authorizeUrlTemplate = new UriTemplate(authorizeUrl);
 		this.accessTokenUrl = accessTokenUrl;
-		this.oauthConsumerSupport = new CoreOAuthConsumerSupport();
 	}
 
 	public OAuthToken fetchNewRequestToken(String callbackUrl) {
-		OAuthConsumerToken requestToken = getOAuthConsumerSupport().getUnauthorizedRequestToken(
-				getProtectedResourceDetails(), callbackUrl);
-		return new OAuthToken(requestToken.getValue(), requestToken.getSecret());
+		Map<String, String> requestTokenParameters = new HashMap<String, String>();
+		requestTokenParameters.put("oauth_callback", callbackUrl);
+		return getTokenFromProvider(requestTokenParameters, requestTokenUrl);
 	}
 
 	public String buildAuthorizeUrl(String requestToken) {
@@ -61,24 +73,102 @@ public class OAuth1Template implements OAuth1Operations {
 	}
 
 	public OAuthToken exchangeForAccessToken(AuthorizedRequestToken requestToken) {
-		OAuthConsumerToken consumerToken = new OAuthConsumerToken();
-		consumerToken.setValue(requestToken.getValue());
-		consumerToken.setSecret(requestToken.getSecret());
-		OAuthConsumerToken accessToken = getOAuthConsumerSupport().getAccessToken(getProtectedResourceDetails(),
-				consumerToken, requestToken.getVerifier());
-		return new OAuthToken(accessToken.getValue(), accessToken.getSecret());
+		Map<String, String> accessTokenParameters = new HashMap<String, String>();
+		accessTokenParameters.put("oauth_token", requestToken.getValue());
+		accessTokenParameters.put("oauth_verifier", requestToken.getVerifier());
+		return getTokenFromProvider(accessTokenParameters, accessTokenUrl);
 	}
 
-	private ProtectedResourceDetails getProtectedResourceDetails() {
-		BaseProtectedResourceDetails details = new BaseProtectedResourceDetails();
-		details.setConsumerKey(consumerKey);
-		details.setSharedSecret(new SharedConsumerSecret(consumerSecret));
-		details.setRequestTokenURL(requestTokenUrl);
-		details.setAccessTokenURL(accessTokenUrl);
-		return details;
+	// private helpers
+	private String buildAuthorizationHeader(String targetUrl, Map<String, String> parameters, HttpMethod method,
+			String tokenSecret) {
+		String baseString = buildBaseString(targetUrl, parameters, method);
+		String signature = calculateSignature(baseString, tokenSecret);
+		String header = "OAuth ";
+		for (String key : parameters.keySet()) {
+			header += key + "=\"" + encode(parameters.get(key)) + "\", ";
+		}
+		header += "oauth_signature=\"" + encode(signature) + "\"";
+		return header;
 	}
 
-	protected OAuthConsumerSupport getOAuthConsumerSupport() {
-		return oauthConsumerSupport;
+	private OAuthToken getTokenFromProvider(Map<String, String> tokenRequestParameters, String tokenUrl) {
+		Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("oauth_consumer_key", consumerKey);
+		parameters.put("oauth_signature_method", HMAC_SHA1_SIGNATURE_NAME);
+		parameters.put("oauth_timestamp", String.valueOf(System.currentTimeMillis() / 1000));
+		parameters.put("oauth_nonce", UUID.randomUUID().toString());
+		parameters.put("oauth_version", "1.0");
+		parameters.putAll(tokenRequestParameters);
+		String authHeader = buildAuthorizationHeader(tokenUrl, parameters, HttpMethod.POST, null);
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+		headers.add("Authorization", authHeader);
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+
+		ResponseEntity<String> response = new RestTemplate().exchange(tokenUrl, HttpMethod.POST, request, String.class);
+
+		Map<String, String> responseMap = parseResponse(response.getBody());
+		return new OAuthToken(responseMap.get("oauth_token"), responseMap.get("oauth_token_secret"));
 	}
+
+	private Map<String, String> parseResponse(String response) {
+		Map<String, String> responseMap = new HashMap<String, String>();
+		String[] responseEntries = response.split("&");
+		for (String entry : responseEntries) {
+			String[] keyValuePair = entry.split("=");
+			if (keyValuePair.length > 1) {
+				responseMap.put(keyValuePair[0], keyValuePair[1]);
+			}
+		}
+		return responseMap;
+	}
+
+	private String buildBaseString(String targetUrl, Map<String, String> parameters, HttpMethod method) {
+
+		String baseString = method.toString() + "&" + encode(targetUrl) + "&";
+		List<String> keys = new ArrayList<String>(parameters.keySet());
+		Collections.sort(keys);
+		String separator = "";
+		for (String key : keys) {
+			baseString += separator + encode(key) + "%3D" + encode(encode(parameters.get(key)));
+			separator = "%26";
+		}
+
+		return baseString;
+	}
+
+	private String calculateSignature(String baseString, String tokenSecret) {
+		String key = consumerSecret + "&" + (tokenSecret == null ? "" : tokenSecret);
+		return sign(baseString, key);
+	}
+
+	private String sign(String signatureBaseString, String key) {
+		try {
+			Mac mac = Mac.getInstance(HMAC_SHA1_MAC_NAME);
+			SecretKeySpec spec = new SecretKeySpec(key.getBytes(), HMAC_SHA1_MAC_NAME);
+			mac.init(spec);
+			byte[] text = signatureBaseString.getBytes("UTF-8");
+			byte[] signatureBytes = mac.doFinal(text);
+			signatureBytes = Base64.encodeBase64(signatureBytes);
+			String signature = new String(signatureBytes, "UTF-8");
+			return signature;
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalStateException(e);
+		} catch (InvalidKeyException e) {
+			throw new IllegalStateException(e);
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private String encode(String in) {
+		try {
+			return URLEncoder.encode(in, "UTF-8");
+		} catch (Exception wontHappen) {
+			return null;
+		}
+	}
+
+	private static final String HMAC_SHA1_SIGNATURE_NAME = "HMAC-SHA1";
+	private static final String HMAC_SHA1_MAC_NAME = "HmacSHA1";
 }
