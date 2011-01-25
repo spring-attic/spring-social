54dfd22de00fba54db6f98866056a7121a92841c
Keith Donald <keith.donald@springsource.com>
2011-01-25 17:33:20 -0500

    oauth 2 service provider test

--- spring-social-client/src/test/java/org/springframework/social/provider/oauth2/OAuth2ServiceProviderTest.java
+++ spring-social-client/src/test/java/org/springframework/social/provider/oauth2/OAuth2ServiceProviderTest.java
@@ -1,4 +1,4 @@
-package org.springframework.social.provider.oauth1;
+package org.springframework.social.provider.oauth2;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -6,45 +6,40 @@ import static org.junit.Assert.fail;
 import java.util.List;
 
 import org.junit.Test;
-import org.springframework.security.oauth.client.oauth1.AuthorizedRequestToken;
-import org.springframework.security.oauth.client.oauth1.OAuth1Operations;
-import org.springframework.security.oauth.client.oauth1.OAuthToken;
+import org.springframework.security.oauth.client.oauth2.AccessGrant;
+import org.springframework.security.oauth.client.oauth2.OAuth2Operations;
 import org.springframework.social.provider.AuthorizationProtocol;
 import org.springframework.social.provider.ServiceProviderConnection;
 import org.springframework.social.provider.support.ConnectionRepository;
 import org.springframework.social.provider.test.StubConnectionRepository;
 
-public class OAuth1ServiceProviderTest {
+public class OAuth2ServiceProviderTest {
 
 	private ConnectionRepository connectionRepository = new StubConnectionRepository();
 
-	private OAuth1ServiceProvider<TestApi> serviceProvider = new TestServiceProvider("54321", "65432", connectionRepository);
+	private OAuth2ServiceProvider<TestApi> serviceProvider = new TestServiceProvider("54321", "65432", connectionRepository);
 
 	@Test
 	public void connectFlow() {
 		// preconditions
-		assertEquals(AuthorizationProtocol.OAUTH_1, serviceProvider.getAuthorizationProtocol());
+		assertEquals(AuthorizationProtocol.OAUTH_2, serviceProvider.getAuthorizationProtocol());
 		Long accountId = 1L;
 		assertEquals(false, serviceProvider.isConnected(accountId));
 		assertEquals(0, serviceProvider.getConnections(accountId).size());
 		
-		// oauth dance
-		OAuth1Operations oauthClient = serviceProvider.getOAuth1Operations();
-		OAuthToken requestToken = oauthClient.fetchNewRequestToken("http://localhost:8080/me");
-		String authorizeUrl = oauthClient.buildAuthorizeUrl(requestToken.getValue());
-		assertEquals("http://springsource.org/oauth/authorize?request_token=12345", authorizeUrl);
-		OAuthToken accessToken = oauthClient.exchangeForAccessToken(new AuthorizedRequestToken(requestToken, "verifier"));
+		// oauth 2 dance
+		OAuth2Operations oauthClient = serviceProvider.getOAuth2Operations();
+		String authorizeUrl = oauthClient.buildAuthorizeUrl("http://localhost:8080/me", "READ_WRITE");
+		assertEquals("http://springsource.org/oauth/authorize?scope=READ_WRITE", authorizeUrl);
+		AccessGrant accessGrant = oauthClient.exchangeForAccess("authorizationGrant", "http://localhost:8080/me");
 
 		// connect
-		ServiceProviderConnection<TestApi> connection = serviceProvider.connect(accountId, accessToken);
+		ServiceProviderConnection<TestApi> connection = serviceProvider.connect(accountId, accessGrant);
 		TestApi api = connection.getApi();
 		assertEquals("Hello Keith!", api.testOperation("Keith"));
 
 		TestApiImpl impl = (TestApiImpl) api;
-		assertEquals("54321", impl.getConsumerKey());
-		assertEquals("65432", impl.getConsumerSecret());
-		assertEquals("34567", impl.getAccessToken());
-		assertEquals("45678", impl.getSecret());
+		assertEquals("12345", impl.getAccessToken());
 		
 		// postconditions
 		assertEquals(true, serviceProvider.isConnected(accountId));
@@ -77,7 +72,7 @@ public class OAuth1ServiceProviderTest {
 	@Test
 	public void duplicateConnection() {
 		Long accountId = 1L;
-		OAuthToken accessToken = new OAuthToken("12345", "23456");
+		AccessGrant accessToken = new AccessGrant("12345", "23456");
 		serviceProvider.connect(accountId, accessToken);
 		try {
 			serviceProvider.connect(accountId, accessToken);
@@ -87,14 +82,14 @@ public class OAuth1ServiceProviderTest {
 		}
 	}
 	
-	static class TestServiceProvider extends AbstractOAuth1ServiceProvider<TestApi> {
+	static class TestServiceProvider extends AbstractOAuth2ServiceProvider<TestApi> {
 
 		public TestServiceProvider(String consumerKey, String consumerSecret, ConnectionRepository connectionRepository) {
-			super("test", connectionRepository, consumerKey, consumerSecret, new StubOAuth1Operations());
+			super("test", connectionRepository, new StubOAuth2Operations());
 		}
 
-		protected TestApi getApi(String consumerKey, String consumerSecret, String accessToken, String secret) {
-			return new TestApiImpl(consumerKey, consumerSecret, accessToken, secret);
+		protected TestApi getApi(String accessToken) {
+			return new TestApiImpl(accessToken);
 		}
 
 	}
@@ -105,40 +100,20 @@ public class OAuth1ServiceProviderTest {
 	
 	static class TestApiImpl implements TestApi {
 
-		private String consumerKey;
-		
-		private String consumerSecret;
-		
 		private String accessToken;
 		
-		private String secret;
-		
-		public TestApiImpl(String consumerKey, String consumerSecret, String accessToken, String secret) {
-			this.consumerKey = consumerKey;
-			this.consumerSecret = consumerSecret;
+		public TestApiImpl(String accessToken) {
 			this.accessToken = accessToken;
-			this.secret = secret;
 		}
-
-		public String getConsumerKey() {
-			return consumerKey;
-		}
-
-		public String getConsumerSecret() {
-			return consumerSecret;
-		}
-
+		
 		public String getAccessToken() {
 			return accessToken;
 		}
-
-		public String getSecret() {
-			return secret;
-		}
-
+		
 		public String testOperation(String arg) {
 			return "Hello " + arg + "!";
 		}
 		
 	}
+
 }
