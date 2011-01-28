304545efab18fb31d3c3cee0a855a1566c8f9a9f
Craig Walls <cwalls@vmware.com>
2011-01-28 11:27:01 -0600

    Fixed test dependency in config module

--- spring-social-config/src/test/java/org/springframework/social/config/xml/SocialNamespaceHandlerTest.java
+++ spring-social-config/src/test/java/org/springframework/social/config/xml/SocialNamespaceHandlerTest.java
@@ -15,8 +15,7 @@
  */
 package org.springframework.social.config.xml;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.lang.reflect.Field;
 
