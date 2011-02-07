9338a77882fb5f54d4a95d7a74c763f51b4da2e3
Craig Walls <cwalls@vmware.com>
2011-02-07 12:30:42 -0600

    Filled out test for FacebookTemplate using mock rest server.

--- spring-social-facebook/src/test/java/org/springframework/social/facebook/FacebookTemplateTest.java
+++ spring-social-facebook/src/test/java/org/springframework/social/facebook/FacebookTemplateTest.java
@@ -15,8 +15,20 @@
  */
 package org.springframework.social.facebook;
 
+import static org.junit.Assert.*;
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.web.client.test.RequestMatchers.*;
+import static org.springframework.web.client.test.ResponseCreators.*;
+
+import java.net.URI;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.test.MockRestServiceServer;
 
 /**
  * @author Craig Walls
@@ -26,10 +38,15 @@ public class FacebookTemplateTest {
 	private static final String ACCESS_TOKEN = "someAccessToken";
 	
 	private FacebookTemplate facebook;
+	private MockRestServiceServer mockServer;
+	private HttpHeaders responseHeaders;
 
 	@Before
 	public void setup() {
 		facebook = new FacebookTemplate(ACCESS_TOKEN);
+		mockServer = MockRestServiceServer.createServer(facebook.getRestTemplate());
+		responseHeaders = new HttpHeaders();
+		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
 	}
 
 	// TODO complete with testing of json response reading/request writing behavior
@@ -37,34 +54,58 @@ public class FacebookTemplateTest {
 	
 	@Test
 	public void getFriendIds() {
+		mockServer.expect(requestTo("https://graph.facebook.com/me/friends")).andExpect(method(GET))
+				.andRespond(withResponse(new ClassPathResource("friends.json", getClass()), responseHeaders));
+
+		List<String> friendIds = facebook.getFriendIds();
+		assertEquals(3, friendIds.size());
+		assertTrue(friendIds.contains("12345"));
+		assertTrue(friendIds.contains("67890"));
+		assertTrue(friendIds.contains("24680"));
 	}
 
 	@Test
 	public void getUserProfile() {
+		mockServer.expect(requestTo("https://graph.facebook.com/me")).andExpect(method(GET))
+				.andRespond(withResponse(new ClassPathResource("me.json", getClass()), responseHeaders));
+
+		FacebookProfile profile = facebook.getUserProfile();
+		assertEquals(123456789, profile.getId());
+		assertEquals("Craig", profile.getFirstName());
+		assertEquals("Walls", profile.getLastName());
+		assertEquals("Craig Walls", profile.getName());
+		assertEquals("cwalls@vmware.com", profile.getEmail());
+		assertEquals("http://www.facebook.com/profile.php?id=123456789", profile.getProfileUrl());
+		assertEquals("https://graph.facebook.com/123456789/picture", profile.getProfileImageUrl());
 	}
 
 	@Test
 	public void getProfileId() {
+		mockServer.expect(requestTo("https://graph.facebook.com/me")).andExpect(method(GET))
+				.andRespond(withResponse(new ClassPathResource("me.json", getClass()), responseHeaders));
+
+		assertEquals("123456789", facebook.getProfileId());
 	}
 
 	@Test
-	public void getProfileUrl() {
+	public void updateStatus() throws Exception {
+		responseHeaders.setLocation(new URI("http://www.facebook.com/me"));
+		String requestBody = "message=Hello+Facebook+World";
+		mockServer.expect(requestTo("https://graph.facebook.com/me/feed")).andExpect(method(POST))
+				.andExpect(body(requestBody)).andRespond(withResponse("", responseHeaders));
+		facebook.updateStatus("Hello Facebook World");
+		mockServer.verify();
 	}
 
 	@Test
-	public void getProfilePicture() {
-	}
-
-	@Test
-	public void getProfilePicture_anotherUser() {
-	}
-
-	@Test
-	public void updateStatus() {
-	}
-
-	@Test
-	public void updateStatus_withLink() {
+	public void updateStatus_withLink() throws Exception {
+		responseHeaders.setLocation(new URI("http://www.facebook.com/me"));
+		String requestBody = "link=someLink&name=some+name&caption=some+caption&description=some+description&message=Hello+Facebook+World";
+		mockServer.expect(requestTo("https://graph.facebook.com/me/feed")).andExpect(method(POST))
+				.andExpect(body(requestBody)).andRespond(withResponse("", responseHeaders));
+		FacebookLink link = new FacebookLink("someLink", "some name", "some caption", "some description");
+		facebook.updateStatus("Hello Facebook World", link);
+		mockServer.verify();
 	}
 	
 }
