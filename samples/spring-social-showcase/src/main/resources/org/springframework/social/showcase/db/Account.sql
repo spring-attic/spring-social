a0f5fa424b2253c6fe237e663cf4281f8a24584e
Craig Walls <cwalls@vmware.com>
2011-02-14 15:33:57 -0600

    Removed samples from main repository. Moved to separate samples repository at http://git.springsource.org/spring-social/samples

--- samples/spring-social-showcase/src/main/resources/org/springframework/social/showcase/db/Account.sql
+++
@@ -1,6 +0,0 @@
-create table ShowcaseUser (	id identity,
-							username varchar unique,
-							password varchar not null,
-							firstName varchar not null, 
-							lastName varchar not null,
-							primary key (id));
--- samples/spring-social-showcase/src/main/resources/org/springframework/social/showcase/db/data.sql
+++
@@ -1,4 +0,0 @@
-insert into ShowcaseUser (username, password, firstName, lastName) values ('habuma', 'freebirds', 'Craig', 'Walls');
-insert into ShowcaseUser (username, password, firstName, lastName) values ('kdonald', 'melbourne', 'Keith', 'Donald');
-insert into ShowcaseUser (username, password, firstName, lastName) values ('rclarkson', 'atlanta', 'Roy', 'Clarkson');
-
