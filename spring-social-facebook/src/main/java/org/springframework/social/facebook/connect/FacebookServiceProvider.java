03b308d622200f77dab9827500e40a50bb3491d7
Keith Donald <keith.donald@springsource.com>
2011-02-24 10:16:08 -0500

    exposing appId and appSecret properties

--- spring-social-facebook/src/main/java/org/springframework/social/facebook/connect/FacebookServiceProvider.java
+++ spring-social-facebook/src/main/java/org/springframework/social/facebook/connect/FacebookServiceProvider.java
@@ -27,8 +27,12 @@ import org.springframework.social.oauth2.OAuth2Template;
  */
 public final class FacebookServiceProvider extends AbstractOAuth2ServiceProvider<FacebookApi> {
 
-	public FacebookServiceProvider(String clientId, String clientSecret, ConnectionRepository connectionRepository) {
-		super("facebook", connectionRepository, new OAuth2Template(clientId, clientSecret,
+	private String appId;
+	
+	private String appSecret;
+	
+	public FacebookServiceProvider(String appId, String appSecret, ConnectionRepository connectionRepository) {
+		super("facebook", connectionRepository, new OAuth2Template(appId, appSecret,
 				"https://graph.facebook.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}",
 				"https://graph.facebook.com/oauth/access_token"));
 	}
@@ -43,13 +47,14 @@ public final class FacebookServiceProvider extends AbstractOAuth2ServiceProvider
 		return api.getProfileId();
 	}
 
+	// properties needed to access Facebook JavaScript API
+	
+	public String getAppId() {
+		return appId;
+	}
+	
 	public String getAppSecret() {
-		// TODO Auto-generated method stub
-		return null;
+		return appSecret;
 	}
 
-	public String getAppId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
