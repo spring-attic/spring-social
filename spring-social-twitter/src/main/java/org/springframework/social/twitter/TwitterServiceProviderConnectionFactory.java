7c9b899da338ad5001fdec1346ed3d0caecb804b
Keith Donald <keith.donald@springsource.com>
2011-03-30 22:51:45 -0400

    testing connection factory model with twitter

---
+++ spring-social-twitter/src/main/java/org/springframework/social/twitter/TwitterServiceProviderConnectionFactory.java
@@ -0,0 +1,30 @@
+package org.springframework.social.twitter;
+
+import org.springframework.social.connect.OAuth1ServiceProviderConnectionFactory;
+import org.springframework.social.connect.spi.ProviderProfile;
+import org.springframework.social.connect.spi.ServiceApiAdapter;
+
+public class TwitterServiceProviderConnectionFactory extends OAuth1ServiceProviderConnectionFactory<TwitterApi> {
+
+	public TwitterServiceProviderConnectionFactory(String consumerKey, String consumerSecret) {
+		super("twitter", new TwitterServiceProvider(consumerKey, consumerSecret), new TwitterServiceApiAdapter(), true);
+	}
+	
+	private static class TwitterServiceApiAdapter implements ServiceApiAdapter<TwitterApi> {
+
+		public boolean test(TwitterApi serviceApi) {
+			return true;
+		}
+
+		public ProviderProfile getProfile(TwitterApi serviceApi) {
+			TwitterProfile profile = serviceApi.getUserProfile();
+			return new ProviderProfile(Long.toString(profile.getId()), profile.getScreenName(), profile.getProfileUrl(), profile.getProfileImageUrl());
+		}
+
+		public void updateStatus(TwitterApi serviceApi, String message) {
+			serviceApi.updateStatus(message);	
+		}
+
+	}
+	
+}
