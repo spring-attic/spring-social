1f0bccd43105939001cd85105a20adb0b3a05f5a
Keith Donald <keith.donald@springsource.com>
2011-04-26 23:23:08 -0400

    trip it classname updates

---
+++ spring-social-gowalla/src/test/java/org/springframework/social/gowalla/connect/GowallaApiAdapterTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.social.gowalla.connect;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.social.connect.UserProfile;
+import org.springframework.social.gowalla.api.GowallaApi;
+import org.springframework.social.gowalla.api.GowallaProfile;
+
+public class GowallaApiAdapterTest {
+
+	private GowallaApiAdapter apiAdapter = new GowallaApiAdapter();
+	
+	private GowallaApi api = Mockito.mock(GowallaApi.class);
+	
+	@Test
+	public void fetchProfile() {		
+		Mockito.when(api.getUserProfile()).thenReturn(new GowallaProfile("habuma", "Craig", "Walls", "Plano, TX", 1, 2, "http://s3.amazonaws.com/static.gowalla.com/users/362641-standard.jpg?1294162106"));
+		UserProfile profile = apiAdapter.fetchUserProfile(api);
+		assertEquals("Craig Walls", profile.getName());
+		assertEquals("Craig", profile.getFirstName());
+		assertEquals("Walls", profile.getLastName());
+		assertNull(profile.getEmail());
+		assertEquals("habuma", profile.getUsername());
+	}
+	
+}
---
+++ spring-social-tripit/src/test/java/org/springframework/social/tripit/connect/TripItApiAdapterTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.social.tripit.connect;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.social.connect.UserProfile;
+import org.springframework.social.tripit.api.TripItApi;
+import org.springframework.social.tripit.api.TripItProfile;
+
+public class TripItApiAdapterTest {
+
+	private TripItApiAdapter apiAdapter = new TripItApiAdapter();
+	
+	private TripItApi api = Mockito.mock(TripItApi.class);
+	
+	@Test
+	public void fetchProfile() {
+		Mockito.when(api.getUserProfile()).thenReturn(new TripItProfile("habuma", "habuma", "Craig Walls", "cwalls@vmware.com", "Plano, TX", "SpringSource", "people/habuma", "http://static.tripit.com/uploads/images/0/0/6/006b210269799fa70ff6ae2c0cdb8a41e9c.jpg"));
+		UserProfile profile = apiAdapter.fetchUserProfile(api);
+		assertEquals("Craig Walls", profile.getName());
+		assertEquals("Craig", profile.getFirstName());
+		assertEquals("Walls", profile.getLastName());
+		assertEquals("cwalls@vmware.com", profile.getEmail());
+		assertEquals("habuma", profile.getUsername());
+	}
+
+	@Test
+	public void fetchProfileFirstNameOnly() {
+		Mockito.when(api.getUserProfile()).thenReturn(new TripItProfile("habuma", "habuma", "Craig", "cwalls@vmware.com", "Plano, TX", "SpringSource", "people/habuma", "http://static.tripit.com/uploads/images/0/0/6/006b210269799fa70ff6ae2c0cdb8a41e9c.jpg"));
+		UserProfile profile = apiAdapter.fetchUserProfile(api);
+		assertEquals("Craig", profile.getName());
+		assertEquals("Craig", profile.getFirstName());
+		assertNull(profile.getLastName());
+		assertEquals("cwalls@vmware.com", profile.getEmail());
+		assertEquals("habuma", profile.getUsername());
+	}
+
+	@Test
+	public void fetchProfileMiddleName() {
+		Mockito.when(api.getUserProfile()).thenReturn(new TripItProfile("habuma", "habuma", "Michael Craig Walls", "cwalls@vmware.com", "Plano, TX", "SpringSource", "people/habuma", "http://static.tripit.com/uploads/images/0/0/6/006b210269799fa70ff6ae2c0cdb8a41e9c.jpg"));
+		UserProfile profile = apiAdapter.fetchUserProfile(api);
+		assertEquals("Michael Craig Walls", profile.getName());
+		assertEquals("Michael", profile.getFirstName());
+		assertEquals("Walls", profile.getLastName());
+		assertEquals("cwalls@vmware.com", profile.getEmail());
+		assertEquals("habuma", profile.getUsername());
+	}
+	
+	@Test
+	public void fetchProfileExtraWhitespace() {
+		Mockito.when(api.getUserProfile()).thenReturn(new TripItProfile("habuma", "habuma", "Michael    Craig Walls", "cwalls@vmware.com", "Plano, TX", "SpringSource", "people/habuma", "http://static.tripit.com/uploads/images/0/0/6/006b210269799fa70ff6ae2c0cdb8a41e9c.jpg"));
+		UserProfile profile = apiAdapter.fetchUserProfile(api);
+		assertEquals("Michael    Craig Walls", profile.getName());
+		assertEquals("Michael", profile.getFirstName());
+		assertEquals("Walls", profile.getLastName());
+		assertEquals("cwalls@vmware.com", profile.getEmail());
+		assertEquals("habuma", profile.getUsername());
+	}
+	
+}
