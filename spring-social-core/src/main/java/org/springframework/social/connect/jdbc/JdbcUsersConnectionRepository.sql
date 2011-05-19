de1b1acbb097f4739bdf34f5f32cb8bc78b3e3d7
straxus <straxus@gmail.com>
2011-05-18 18:59:03 -0700

    Added MySQL 5.1-compliant schema to this file - not sure what DB the previous syntax was for, but it's not MySQL. I've basically guessed at the varchar and int sizes to use - please adjust as appropriate to match expected data size.

--- spring-social-core/src/main/java/org/springframework/social/connect/jdbc/JdbcUsersConnectionRepository.sql
+++ spring-social-core/src/main/java/org/springframework/social/connect/jdbc/JdbcUsersConnectionRepository.sql
@@ -11,3 +11,21 @@ create table UserConnection (userId varchar not null,
 					expireTime bigint,
 					primary key (userId, providerId, providerUserId));
 create unique index UserConnectionRank on UserConnection(userId, providerId, rank);
+
+/* MySQL 5.1-compliant schema */
+create table UserConnection (
+ userId varchar(64) not null,
+ providerId varchar(255) not null,
+ providerUserId varchar(255),
+ rank int not null,
+ displayName varchar(1024),
+ profileUrl varchar(4096),
+ imageUrl varchar(4096),
+ accessToken varchar(4096) not null,					
+ secret varchar(4096),
+ refreshToken varchar(4096),
+ expireTime bigint(20),
+ primary key (userId, providerId, providerUserId)
+);
+
+create unique index UserConnectionRank on UserConnection(userId, providerId, rank);
