7f811437da2e1d2ff11ffa1c7c8232909af7ef15
Keith Donald <keith.donald@springsource.com>
2011-07-19 09:27:24 -0400

    polish

--- spring-social-web/src/test/java/org/springframework/social/connect/web/ConnectSupportTest.java
+++ spring-social-web/src/test/java/org/springframework/social/connect/web/ConnectSupportTest.java
@@ -15,7 +15,8 @@
  */
 package org.springframework.social.connect.web;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.net.URL;
 
