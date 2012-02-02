737cc7c547c331c6a54288906e5bf39b4947caa2
Craig Walls <cwalls@vmware.com>
2012-02-02 10:01:11 -0600

    Moved tests that were previously housed in spring-social-test into spring-social-core.

---
+++ spring-social-core/src/test/java/org/springframework/social/connect/oauth2/StubOAuth2Operations.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.social.connect.oauth2;
+
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.GrantType;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.util.MultiValueMap;
+
+class StubOAuth2Operations implements OAuth2Operations {
+
+	public String buildAuthorizeUrl(GrantType grantType, OAuth2Parameters parameters) {
+		return "http://springsource.org/oauth/authorize?scope=" + parameters.getScope();
+	}
+	
+	public String buildAuthenticateUrl(GrantType grantType, OAuth2Parameters parameters) {
+		return buildAuthorizeUrl(grantType, parameters);
+	}
+
+	public AccessGrant exchangeForAccess(String authorizationGrant, String redirectUri, MultiValueMap<String, String> additionalParameters) {
+		return new AccessGrant("12345", null, "23456", 3600);
+	}
+	
+	public AccessGrant refreshAccess(String refreshToken, String scope, MultiValueMap<String, String> additionalParameters) {
+		return new AccessGrant("12345", null,  "23456", 3600);
+	}
+}
