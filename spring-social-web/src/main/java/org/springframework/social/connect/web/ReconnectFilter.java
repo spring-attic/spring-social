023e31fe23c194a0be1e624fc2cc0441d0ec7473
Craig Walls <cwalls@vmware.com>
2013-02-20 17:20:08 -0600

    SOCIAL-328: Changed filter to handle /connect/{provider ID}?reconnect instead of /refresh/{provider ID}

--- spring-social-web/src/main/java/org/springframework/social/connect/web/ReconnectFilter.java
+++ spring-social-web/src/main/java/org/springframework/social/connect/web/ReconnectFilter.java
@@ -32,17 +32,26 @@ import org.springframework.social.InsufficientPermissionException;
 import org.springframework.social.NotAuthorizedException;
 import org.springframework.social.OperationNotPermittedException;
 import org.springframework.social.UserIdSource;
+import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.util.Assert;
 import org.springframework.web.filter.GenericFilterBean;
 
 /**
- * Servlet filter that intercepts Spring Social {@link ApiException}s thrown in the course of a request and attempts to reconcile any connection-related
- * problems by deleting the stale/revoked connection and walking the user through the connection process to obtain a new connection.
+ * <p>Servlet filter that intercepts Spring Social {@link ApiException}s thrown in the course of a request and attempts to reconcile any connection-related
+ * problems by deleting the stale/revoked connection and walking the user through the connection process to obtain a new connection.</p>
+ * 
+ * <p>This filter handles the exceptions via the following flow:</p>
+ * 
+ * <ul>
+ * <li>If an exception is thrown, redirects to /connect/{provider ID}?reconnect=true</li>
+ * </li>Handles its own redirect to /connect/{provider ID}?reconnect=true and converts the request to a POST request to {@link ConnectController} to kick of the authorization flow.
+ * </ul>
+ * 
  * @author Craig Walls
  */
 public class ReconnectFilter extends GenericFilterBean {
-
-	// TODO: There's a lot of URL manipulation in this case that could probably be broken down into reusable bits
 	
 	private final static Log logger = LogFactory.getLog(ReconnectFilter.class);
 
@@ -50,23 +59,20 @@ public class ReconnectFilter extends GenericFilterBean {
 
 	private UsersConnectionRepository usersConnectionRepository;
 	
-	private String filterRefreshUrl = "/refresh";
-
 	private UserIdSource userIdSource;
 
+	/**
+	 * Creates an instance of {@link ReconnectFilter}.
+	 * @param usersConnectionRepository a {@link UsersConnectionRepository} used to create a {@link ConnectionRepository} for the current user.
+	 * @param userIdSource an instance of {@link UserIdSource} to obtain the current user's ID used to create a {@link ConnectionFactory}.
+	 */
 	public ReconnectFilter(UsersConnectionRepository usersConnectionRepository, UserIdSource userIdSource) {
+		Assert.notNull(usersConnectionRepository, "UsersConnectionRepository cannot be null");
+		Assert.notNull(userIdSource, "UserIdSource cannot be null");
 		this.usersConnectionRepository = usersConnectionRepository;
 		this.userIdSource = userIdSource;
 	}
 
-	/**
-	 * The URL path that this filter will intercept to trigger a reconnect flow.
-	 * Defaults to "/refresh".
-	 */
-	public void setFilterRefreshUrl(String filterRefreshUrl) {
-		this.filterRefreshUrl = filterRefreshUrl;
-	}
-	
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
@@ -95,7 +101,8 @@ public class ReconnectFilter extends GenericFilterBean {
 	// subclassing hooks
 	/**
 	 * Returns the URL to redirect to if it is determined that a connection needs to be renewed.
-	 * By default, the URL will be based on {@link #filterRefreshUrl}, to immediately trigger a connection flow with {@link ConnectController}.
+	 * By default, the filter will redirect to /connect/{provider ID} with a "reconnect" query parameter.
+	 * This filter also handles GET requests to that same path before submitting a POST request to {@link ConnectController} for authorization.
 	 * May be overridden by a subclass to handle other flows, such as redirecting to a page that informs the user that a new connection is needed. 
 	 * @param request The HTTP request that triggered the exception.
 	 * @param apiException The {@link ApiException}.
@@ -103,7 +110,19 @@ public class ReconnectFilter extends GenericFilterBean {
 	 */
 	protected String getRefreshUrl(HttpServletRequest request, ApiException apiException) {
 		String scopeNeeded = getRequiredScope(apiException);				
-		return request.getContextPath() + filterRefreshUrl + "/" + apiException.getProviderId() + (scopeNeeded != null ? "?scope=" + scopeNeeded : "");
+		return request.getContextPath() + "/connect/" + apiException.getProviderId() + "?reconnect=true" + (scopeNeeded != null ? "&scope=" + scopeNeeded : "");
+	}
+
+	/**
+	 * Determines whether or not the handled request should be converted to a POST request to {@link ConnectController} for authorization.
+	 * By default, will return true if the request is a GET request for /connect/{provider ID} and there is a "reconnect" query parameter.
+	 * May be overridden by a subclass to consider other criteria in deciding whether or not to convert the request.
+	 * @param request the handled request.
+	 * @return true if the request should be converted to a POST request to {@link ConnectController}.
+	 */
+	protected boolean shouldPerformRefreshPostRequest(HttpServletRequest request) {
+		String servletPath = request.getServletPath();
+		return request.getMethod().equalsIgnoreCase("GET") && servletPath != null && servletPath.startsWith("/connect/") && request.getParameter("reconnect") != null;
 	}
 
 	// private helpers
@@ -112,18 +131,11 @@ public class ReconnectFilter extends GenericFilterBean {
 	}
 
 	private String getProviderIdFromRequest(HttpServletRequest httpRequest) {
-		return httpRequest.getServletPath().substring(filterRefreshUrl.length()+1);  // TODO: Seems hackish
+		return httpRequest.getServletPath().substring("connect/".length()+1);  // TODO: Seems hackish
 	}
 	
-	private boolean shouldPerformRefreshPostRequest(HttpServletRequest request) {
-		String servletPath = request.getServletPath();
-		return servletPath != null && servletPath.startsWith(filterRefreshUrl) && request.getMethod().equalsIgnoreCase("GET");
-	}
-
 	private void handleExceptionFromFilterChain(Exception e, HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException, ServletException {
-		// Try to extract a SpringSecurityException from the stacktrace
-		Throwable[] causeChain = throwableAnalyzer.determineCauseChain(e);
-		RuntimeException ase = (ApiException) throwableAnalyzer.getFirstThrowableOfType(ApiException.class, causeChain);
+		RuntimeException ase = (ApiException) throwableAnalyzer.getFirstThrowableOfType(ApiException.class, throwableAnalyzer.determineCauseChain(e));
 		if (ase != null && ase instanceof ApiException) {
 			ApiException apiException = (ApiException) ase;
 			logger.info("API Exception: " + e.getMessage());
@@ -141,13 +153,15 @@ public class ReconnectFilter extends GenericFilterBean {
 			throw (RuntimeException) e;
 		}
 
-		// Wrap other Exceptions. This shouldn't actually happen
-		// as we've already covered all the possibilities for doFilter
+		// Wrap other Exceptions in a generic RuntimeException. This should never happen because
+		// we've already covered all the possibilities for doFilter
 		throw new RuntimeException(e);
 	}
 
+	/*
+	 * Request wrapper that converts existing request into a POST request to ConnectController
+	 */
 	private final class ReconnectionPostRequest extends HttpServletRequestWrapper {
-
 		private ReconnectionPostRequest(HttpServletRequest request) {
 			super(request);
 		}
@@ -156,29 +170,6 @@ public class ReconnectFilter extends GenericFilterBean {
 		public String getMethod() {
 			return "POST";
 		}
-		
-		@Override
-		public String getRequestURI() {
-			return getRequestURL().toString();
-		}
-		
-		@Override
-		public StringBuffer getRequestURL() {
-			HttpServletRequest request = (HttpServletRequest) getRequest();
-			return convertRefreshPathToConnectPath(request.getRequestURL());
-		}
-
-		@Override
-		public String getServletPath() {
-			HttpServletRequest request = (HttpServletRequest) getRequest();
-			return convertRefreshPathToConnectPath(new StringBuffer(request.getServletPath())).toString();
-		}
-		
-		private StringBuffer convertRefreshPathToConnectPath(StringBuffer refreshPath) {
-			int filterPathIndex = refreshPath.indexOf(filterRefreshUrl);
-			return refreshPath.replace(filterPathIndex, filterPathIndex + filterRefreshUrl.length(), "/connect");
-		}
-
 	}
 
 }
