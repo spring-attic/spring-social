8a6031d5dbb586a227beeeb639f238e2c9c89655
Craig Walls <cwalls@vmware.com>
2013-05-10 15:13:25 -0500

    Add in-memory implementation of connection repositories. SOCIAL-361

--- spring-social-core/src/main/java/org/springframework/social/connect/mem/InMemoryUsersConnectionRepository.java
+++ spring-social-core/src/main/java/org/springframework/social/connect/mem/InMemoryUsersConnectionRepository.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.social.connect.mem;
 
 import java.util.ArrayList;
@@ -16,6 +31,12 @@ import org.springframework.social.connect.ConnectionSignUp;
 import org.springframework.social.connect.NoSuchConnectionException;
 import org.springframework.social.connect.UsersConnectionRepository;
 
+/**
+ * {@link UsersConnectionRepository} that stores connections in memory.
+ * Intended for testing and small-scale applications as a convenient alternative to JdbcUsersConnectionRepository and an in-memory H2 database.
+ * Not intended for production use, as it will be cleared out when the application exits.
+ * @author Craig Walls
+ */
 public class InMemoryUsersConnectionRepository implements UsersConnectionRepository {
 
 	private ConnectionFactoryLocator connectionFactoryLocator;
