e72fb6a2710a6e981757819dce494f09220ef120
Craig Walls <cwalls@vmware.com>
2013-08-22 11:57:23 -0500

    Polish and discover dependencies from shared ApplicationContext object. SOCIAL-388

--- spring-social-security/src/main/java/org/springframework/social/security/SpringSocialConfigurer.java
+++ spring-social-security/src/main/java/org/springframework/social/security/SpringSocialConfigurer.java
@@ -15,10 +15,13 @@
  */
 package org.springframework.social.security;
 
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import org.springframework.social.UserIdSource;
 import org.springframework.social.connect.UsersConnectionRepository;
@@ -32,48 +35,68 @@ import org.springframework.social.connect.UsersConnectionRepository;
  *   http.
  *     // HTTP security configuration details snipped
  *     .and()
- *        .apply(
- *            new SpringSocialHttpConfigurer(userIdSource(), usersConnectionRepository, authenticationServiceLocator)
- *         );
+ *        .apply(new SpringSocialHttpConfigurer())
+ *        .and().setSharedObject(ApplicationContext.class, context);
  * }
  * </pre>
+ * 
+ * Note that when using with Spring Security 3.2.0.RC1, it's important to set the ApplicationContext as a shared object,
+ * as shown above. Later RCs and the GA release of Spring Security will automatically set the ApplicationContext as a
+ * shared object, making this step unnecessary. 
  * 
  * @author Craig Walls
  */
 public class SpringSocialConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
-	private final UserIdSource userIdSource;
-	
-	private final UsersConnectionRepository usersConnectionRepository;
-	
-	private final SocialAuthenticationServiceLocator authServiceLocator;
-
-	private SocialUserDetailsService socialUsersDetailsService;
+	private UserIdSource userIdSource;
 
 	/**
 	 * Constructs a SpringSocialHttpConfigurer.
-	 * @param userIdSource A {@link UserIdSource}.
-	 * @param usersConnectionRepository A {@link UsersConnectionRepository}.
-	 * @param authServiceLocator A {@link SocialAuthenticationServiceLocator}.
+	 * Requires that {@link UsersConnectionRepository}, {@link SocialAuthenticationServiceLocator}, and
+	 * {@link SocialUserDetailsService} beans be available in the application context.
+	 * If using with Spring Security 3.2.0.RC1, then you must also set the ApplicationContext as a shared object
+	 * (this is unnecessary for later release candidates and the GA release of Spring Security 3.2.0).
 	 */
-	public SpringSocialConfigurer(
-			UserIdSource userIdSource, 
-			UsersConnectionRepository usersConnectionRepository, 
-			SocialAuthenticationServiceLocator authServiceLocator,
-			SocialUserDetailsService socialUsersDetailsService) {
-		this.userIdSource = userIdSource;
-		this.usersConnectionRepository = usersConnectionRepository;
-		this.authServiceLocator = authServiceLocator;
-		this.socialUsersDetailsService = socialUsersDetailsService;
+	public SpringSocialConfigurer() {
 	}
 	
 	@Override
-	public void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {		
+		ApplicationContext applicationContext = http.getSharedObject(ApplicationContext.class);
+		UsersConnectionRepository usersConnectionRepository = getDependency(applicationContext, UsersConnectionRepository.class);
+		SocialAuthenticationServiceLocator authServiceLocator = getDependency(applicationContext, SocialAuthenticationServiceLocator.class);
+		SocialUserDetailsService socialUsersDetailsService = getDependency(applicationContext, SocialUserDetailsService.class);
+		
+		SocialAuthenticationFilter filter = new SocialAuthenticationFilter(
+				http.getSharedObject(AuthenticationManager.class), 
+				userIdSource != null ? userIdSource : new AuthenticationNameUserIdSource(), 
+				usersConnectionRepository, 
+				authServiceLocator);
+
+		RememberMeServices rememberMe = http.getSharedObject(RememberMeServices.class);
+		if(rememberMe != null) {
+			filter.setRememberMeServices(rememberMe);
+		}
 		http.authenticationProvider(
 				new SocialAuthenticationProvider(usersConnectionRepository, socialUsersDetailsService))
-			.addFilterBefore(
-				new SocialAuthenticationFilter(http.getSharedObject(AuthenticationManager.class), userIdSource, usersConnectionRepository, authServiceLocator), 
-				AbstractPreAuthenticatedProcessingFilter.class);
+			.addFilterBefore(postProcess(filter), AbstractPreAuthenticatedProcessingFilter.class);
+	}
+
+	private <T> T getDependency(ApplicationContext applicationContext, Class<T> dependencyType) {
+		try {
+			T dependency = applicationContext.getBean(dependencyType);
+			return dependency;
+		} catch (NoSuchBeanDefinitionException e) {
+			throw new IllegalStateException("SpringSocialConfigurer depends on " + dependencyType.getName() +". No single bean of that type found in application context.", e);
+		}
+	}
+	
+	/**
+	 * Sets the {@link UserIdSource} to use for authentication. Defaults to {@link AuthenticationNameUserIdSource}.
+	 */
+	public SpringSocialConfigurer userIdSource(UserIdSource userIdSource) {
+		this.userIdSource = userIdSource;
+		return this;
 	}
 	
 }
