a6f2de6c7d4f26b290d542a9f349181f3b8b20ee
Craig Walls <cwalls@vmware.com>
2013-10-15 09:30:33 -0500

    Add generic/configurable connection factory and service provider implementations. SOCIAL-396

---
+++ spring-social-core/src/main/java/org/springframework/social/oauth2/GenericOAuth2ConnectionFactory.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.social.oauth2;
+
+import org.springframework.social.connect.ApiAdapter;
+import org.springframework.social.connect.support.OAuth2ConnectionFactory;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Generic implementation of an {@link OAuth2ConnectionFactory} that carries no intrinsic knowledge of the API it will be creating connections against.
+ * Instead, it must be configured with API provider-specific details.
+ * This enables developers to use Spring Social to create connections against any API for which there isn't a ready-to-use connection support and API binding
+ * without creating one-off connection factory and service provider implementations.
+ * The API binding provided by this connection factory is simply a {@link RestOperations}, but the underlying {@link RestTemplate} is configured to automatically
+ * add <code>Authorization</code> headers to all requests.
+ * @author Craig Walls
+ */
+public class GenericOAuth2ConnectionFactory extends OAuth2ConnectionFactory<RestOperations> {
+
+	/**
+	 * Creates an instance of GenericOAuth2ConnectionFactory.
+	 * @param providerId Some String that acts as the unique ID for the API provider.
+	 * @param appId The application's ID/key for the API.
+	 * @param appSecret The application's secret for the API.
+	 * @param authorizeUrl The API's OAuth 2 authorization URL.
+	 * @param accessTokenUrl The API's OAuth 2 access token URL.
+	 * @param apiAdapter A custom implementation of {@link ApiAdapter} used to fetch data when creating the connection.
+	 */
+	public GenericOAuth2ConnectionFactory(
+			String providerId, 
+			String appId, 
+			String appSecret,
+			String authorizeUrl,
+			String accessTokenUrl,
+			ApiAdapter<RestOperations> apiAdapter) {
+		super(providerId, new GenericOAuth2ServiceProvider(appId, appSecret, authorizeUrl, null, accessTokenUrl, false), apiAdapter);
+	}
+
+	/**
+	 * Creates an instance of GenericOAuth2ConnectionFactory for a provider that offers a separate authentication URL.
+	 * @param providerId Some String that acts as the unique ID for the API provider.
+	 * @param appId The application's ID/key for the API.
+	 * @param appSecret The application's secret for the API.
+	 * @param authorizeUrl The API's OAuth 2 authorization URL.
+	 * @param authenticateUrl The API's OAuth 2 authentication URL.
+	 * @param accessTokenUrl The API's OAuth2 access token URL.
+	 * @param apiAdapter A custom implementation of {@link ApiAdapter} used to fetch data when creating the connection.
+	 */
+	public GenericOAuth2ConnectionFactory(
+			String providerId, 
+			String appId, 
+			String appSecret,
+			String authorizeUrl,
+			String authenticateUrl,
+			String accessTokenUrl,
+			ApiAdapter<RestOperations> apiAdapter) {
+		super(providerId, new GenericOAuth2ServiceProvider(appId, appSecret, authorizeUrl, authenticateUrl, accessTokenUrl, false), apiAdapter);
+	}
+
+	/**
+	 * Creates an instance of GenericOAuth2ConnectionFactory.
+	 * @param providerId Some String that acts as the unique ID for the API provider.
+	 * @param appId The application's ID/key for the API.
+	 * @param appSecret The application's secret for the API.
+	 * @param authorizeUrl The API's OAuth 2 authorization URL.
+	 * @param accessTokenUrl The API's OAuth 2 access token URL.
+	 * @param useParametersForClientCredentials
+	 * @param apiAdapter A custom implementation of {@link ApiAdapter} used to fetch data when creating the connection.
+	 */
+	public GenericOAuth2ConnectionFactory(
+			String providerId, 
+			String appId, 
+			String appSecret,
+			String authorizeUrl,
+			String accessTokenUrl,
+			boolean useParametersForClientCredentials,
+			ApiAdapter<RestOperations> apiAdapter) {
+		super(providerId, new GenericOAuth2ServiceProvider(appId, appSecret, authorizeUrl, null, accessTokenUrl, useParametersForClientCredentials), apiAdapter);
+	}
+
+	/**
+	 * Creates an instance of GenericOAuth2ConnectionFactory for a provider that offers a separate authentication URL.
+	 * @param providerId Some String that acts as the unique ID for the API provider.
+	 * @param appId The application's ID/key for the API.
+	 * @param appSecret The application's secret for the API.
+	 * @param authorizeUrl The API's OAuth 2 authorization URL.
+	 * @param authenticateUrl The API's OAuth 2 authentication URL.
+	 * @param accessTokenUrl The API's OAuth2 access token URL.
+	 * @param useParametersForClientCredentials
+	 * @param apiAdapter A custom implementation of {@link ApiAdapter} used to fetch data when creating the connection.
+	 */
+	public GenericOAuth2ConnectionFactory(
+			String providerId, 
+			String appId, 
+			String appSecret,
+			String authorizeUrl,
+			String authenticateUrl,
+			String accessTokenUrl,
+			boolean useParametersForClientCredentials,
+			ApiAdapter<RestOperations> apiAdapter) {
+		super(providerId, new GenericOAuth2ServiceProvider(appId, appSecret, authorizeUrl, authenticateUrl, accessTokenUrl, useParametersForClientCredentials), apiAdapter);
+	}
+}
