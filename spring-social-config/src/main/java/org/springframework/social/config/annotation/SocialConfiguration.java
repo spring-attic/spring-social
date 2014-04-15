eb510e78385662db574ef9473ab05e087af008bc
Craig Walls <cwalls@gopivotal.com>
2014-04-15 16:06:41 -0500

    Allow for multiple social configuration classes.

--- spring-social-config/src/main/java/org/springframework/social/config/annotation/SocialConfiguration.java
+++ spring-social-config/src/main/java/org/springframework/social/config/annotation/SocialConfiguration.java
@@ -15,6 +15,8 @@
  */
 package org.springframework.social.config.annotation;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,41 +41,64 @@ public class SocialConfiguration {
 	@Autowired
 	private Environment environment;
 	
-	private SocialConfigurer socialConfigurer;
+	private List<SocialConfigurer> socialConfigurers;
 
 	@Autowired
-	public void setSocialConfigurer(SocialConfigurer socialConfigurer) {
-		Assert.notNull(socialConfigurer, "One configuration class must implement SocialConfigurer (or subclass SocialConfigurerAdapter)");
-		this.socialConfigurer = socialConfigurer;
+	public void setSocialConfigurers(List<SocialConfigurer> socialConfigurers) {
+		Assert.notNull(socialConfigurers, "At least one configuration class must implement SocialConfigurer (or subclass SocialConfigurerAdapter)");
+		Assert.notEmpty(socialConfigurers, "At least one configuration class must implement SocialConfigurer (or subclass SocialConfigurerAdapter)");
+		this.socialConfigurers = socialConfigurers;
 	}
 
 	@Bean
 	public ConnectionFactoryLocator connectionFactoryLocator() {
 		if (securityEnabled) {
 			SecurityEnabledConnectionFactoryConfigurer cfConfig = new SecurityEnabledConnectionFactoryConfigurer();
-			socialConfigurer.addConnectionFactories(cfConfig, environment);
+			for (SocialConfigurer socialConfigurer : socialConfigurers) {
+				socialConfigurer.addConnectionFactories(cfConfig, environment);
+			}
 			return cfConfig.getConnectionFactoryLocator();
 		} else {
 			DefaultConnectionFactoryConfigurer cfConfig = new DefaultConnectionFactoryConfigurer();
-			socialConfigurer.addConnectionFactories(cfConfig, environment);
+			for (SocialConfigurer socialConfigurer : socialConfigurers) {
+				socialConfigurer.addConnectionFactories(cfConfig, environment);
+			}
 			return cfConfig.getConnectionFactoryLocator();
 		}
 	}
 	
 	@Bean
 	public UserIdSource userIdSource() {
-		return socialConfigurer.getUserIdSource();
+		UserIdSource userIdSource = null;
+		for (SocialConfigurer socialConfigurer : socialConfigurers) {
+			UserIdSource userIdSourceCandidate = socialConfigurer.getUserIdSource();
+			if (userIdSourceCandidate != null) {
+				userIdSource = userIdSourceCandidate;
+				break;
+			}
+		}
+		Assert.notNull(userIdSource, "One configuration class must implement getUserIdSource from SocialConfigurer.");
+		return userIdSource;
 	}
 	
 	@Bean
-	public UsersConnectionRepository usersConnectionRepository() {
-		return socialConfigurer.getUsersConnectionRepository(connectionFactoryLocator());
+	public UsersConnectionRepository usersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+		UsersConnectionRepository usersConnectionRepository = null;
+		for (SocialConfigurer socialConfigurer : socialConfigurers) {
+			UsersConnectionRepository ucrCandidate = socialConfigurer.getUsersConnectionRepository(connectionFactoryLocator);
+			if (ucrCandidate != null) {
+				usersConnectionRepository = ucrCandidate;
+				break;
+			}
+		}
+		Assert.notNull(usersConnectionRepository, "One configuration class must implement getUsersConnectionRepository from SocialConfigurer.");
+		return usersConnectionRepository;
 	}
 
 	@Bean
 	@Scope(value="request", proxyMode=ScopedProxyMode.INTERFACES)
-	public ConnectionRepository connectionRepository() {
-		return usersConnectionRepository().createConnectionRepository(userIdSource().getUserId());
+	public ConnectionRepository connectionRepository(UsersConnectionRepository usersConnectionRepository) {
+		return usersConnectionRepository.createConnectionRepository(userIdSource().getUserId());
 	}
 
 	private static boolean isSocialSecurityAvailable() {
--- spring-social-config/src/main/java/org/springframework/social/config/annotation/SocialConfigurerAdapter.java
+++ spring-social-config/src/main/java/org/springframework/social/config/annotation/SocialConfigurerAdapter.java
@@ -15,17 +15,34 @@
  */
 package org.springframework.social.config.annotation;
 
+import org.springframework.core.env.Environment;
+import org.springframework.social.UserIdSource;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
 
 /**
- * Abstract implementation of {@link SocialConfigurer} with convenient default implementations of some methods.
- * Currently only offers a default implementation of {@link SocialConfigurer#getUsersConnectionRepository(ConnectionFactoryLocator)}.
+ * Abstract implementation of {@link SocialConfigurer} with convenient default implementations of methods.
  * @author Craig Walls
  */
 public abstract class SocialConfigurerAdapter implements SocialConfigurer {
 
+	/**
+	 * Default implementation of {@link #addConnectionFactories(ConnectionFactoryConfigurer, Environment)}.
+	 * Implemented as a no-op, adding no connection factories.
+	 */
+	public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment environment) {
+	}
+	
+	/**
+	 * Default implementation of {@link #getUserIdSource()}.
+	 * Returns null, indicating that this configuration class doesn't provide a UserIdSource (another configuration class must provide one, however).
+	 * @return null
+	 */
+	public UserIdSource getUserIdSource() {
+		return null;
+	}
+	
 	/**
 	 * Default implementation of {@link #getUsersConnectionRepository(ConnectionFactoryLocator)} that creates an in-memory repository.
 	 */
