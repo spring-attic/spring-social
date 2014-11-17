d3a8adac8c972103e8d6dafe76e69f0dd1b78e53
Gim Gyung Jin <fltltmxjs@naver.com>
2014-11-17 18:47:18 +0900

    when you work in test envirment, you could setting AllTrustClient to HttpClient by stting isAllTrust=true

--- spring-social-core/src/main/java/org/springframework/social/support/ClientHttpRequestFactorySelector.java
+++ spring-social-core/src/main/java/org/springframework/social/support/ClientHttpRequestFactorySelector.java
@@ -18,10 +18,21 @@ package org.springframework.social.support;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Properties;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.http.HttpHost;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.protocol.HttpContext;
@@ -31,6 +42,7 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.util.ClassUtils;
+
 
 /**
  * Chooses a request factory. Picks a HttpComponentsClientRequestFactory factory if Apache HttpComponents HttpClient is in the classpath.
@@ -72,6 +84,8 @@ public class ClientHttpRequestFactorySelector {
 
 	public static class HttpComponentsClientRequestFactoryCreator {
 		
+		private static boolean isAllTrust = false;
+		
 		public static ClientHttpRequestFactory createRequestFactory(String proxyHost, int proxyPort) {
 			
 			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory() {
@@ -83,18 +97,54 @@ public class ClientHttpRequestFactorySelector {
 				}
 			};
 			
-			
 			if (proxyHost != null) {
 				HttpHost proxy = new HttpHost(proxyHost, proxyPort);
-				CloseableHttpClient httpClient = HttpClients.custom()
-						.setProxy(proxy)
-						.build();
+				CloseableHttpClient httpClient = isAllTrust ? getAllTrustClient(proxy) : getClient(proxy);
 				requestFactory.setHttpClient(httpClient);
 			}
 			
 			return requestFactory;
 			
 		}
+
+		private static CloseableHttpClient getClient(HttpHost proxy) {
+			return HttpClients.custom()
+					.setProxy(proxy)
+					.build();
+		}
+
+		private static CloseableHttpClient getAllTrustClient(HttpHost proxy) {
+			return HttpClients.custom()
+					.setProxy(proxy)
+					.setSslcontext(getSSLContext())
+					.setHostnameVerifier(SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
+					.build();
+		}
+
+		private static SSLContext getSSLContext() {
+			try {
+				KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			    TrustStrategy allTrust = new TrustStrategy() {
+			        @Override
+			        public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			            return true;
+			        }
+			    };
+			    return SSLContexts.custom().useSSL().loadTrustMaterial(trustStore, allTrust).build();
+			} catch (KeyStoreException e) {
+				e.printStackTrace();
+			} catch (KeyManagementException e) {
+				e.printStackTrace();
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		public static void setAllTrust(boolean isAllTrust) {
+			HttpComponentsClientRequestFactoryCreator.isAllTrust = isAllTrust;
+		}
+		
 	}
 
 }
