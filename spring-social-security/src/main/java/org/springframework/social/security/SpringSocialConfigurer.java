d871b500fa190a923fd89d5f9e2c8d1c4c57ca4f
Florian Lopes <florian.lopes@outlook.com>
2015-04-29 22:40:35 +0200

    Complete SocialAuthenticationFilter Java configuration
    Add support for configuring SocialAuthenticationFilter properties using
    Java configuration (via SpringSocialConfigurer) :
     - signupUrl
     - connectionAddedRedirectUrl
     - defaultFailureUrl
    
    Issue: SOCIAL-401

--- spring-social-security/src/main/java/org/springframework/social/security/SpringSocialConfigurer.java
+++ spring-social-security/src/main/java/org/springframework/social/security/SpringSocialConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,6 +49,12 @@ public class SpringSocialConfigurer extends SecurityConfigurerAdapter<DefaultSec
 	
 	private String postFailureUrl;
 
+	private String signupUrl;
+
+	private String connectionAddedRedirectUrl;
+
+	private String defaultFailureUrl;
+
 	private boolean alwaysUsePostLoginUrl = false;
 
 	/**
@@ -84,6 +90,18 @@ public class SpringSocialConfigurer extends SecurityConfigurerAdapter<DefaultSec
 		
 		if (postFailureUrl != null) {
 			filter.setPostFailureUrl(postFailureUrl);
+		}
+
+		if (signupUrl != null) {
+			filter.setSignupUrl(signupUrl);
+		}
+
+		if (connectionAddedRedirectUrl != null) {
+			filter.setConnectionAddedRedirectUrl(connectionAddedRedirectUrl);
+		}
+
+		if (defaultFailureUrl != null) {
+			filter.setDefaultFailureUrl(defaultFailureUrl);
 		}
 		
 		http.authenticationProvider(
@@ -137,6 +155,36 @@ public class SpringSocialConfigurer extends SecurityConfigurerAdapter<DefaultSec
 	 */
 	public SpringSocialConfigurer postFailureUrl(String postFailureUrl) {
 		this.postFailureUrl = postFailureUrl;
+		return this;
+	}
+
+	/**
+	 * Sets the URL to land on after an authentication failure so that the user can register with the application.
+	 * @param signupUrl the URL to redirect to after an authentication failure
+	 * @return this SpringSocialConfigurer for chained configuration
+	 */
+	public SpringSocialConfigurer signupUrl(String signupUrl) {
+		this.signupUrl = signupUrl;
+		return this;
+	}
+
+	/**
+	 * Sets the URL to land on after an a connection was added.
+	 * @param connectionAddedRedirectUrl the URL to redirect after a connection was added
+	 * @return this SpringSocialConfigurer for chained configuration
+	 */
+	public SpringSocialConfigurer connectionAddedRedirectUrl(String connectionAddedRedirectUrl) {
+		this.connectionAddedRedirectUrl = connectionAddedRedirectUrl;
+		return this;
+	}
+
+	/**
+	 * Sets the URL to redirect to if authentication fails or if authorization is denied by the user.
+	 * @param defaultFailureUrl the URL to redirect to after an authentication fail or authorization deny
+	 * @return this SpringSocialConfigurer for chained configuration
+	 */
+	public SpringSocialConfigurer defaultFailureUrl(String defaultFailureUrl) {
+		this.defaultFailureUrl = defaultFailureUrl;
 		return this;
 	}
 	
