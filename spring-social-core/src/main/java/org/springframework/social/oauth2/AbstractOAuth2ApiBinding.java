b60bc30cd0d19d5f0e7ebb960aa08c83091d0ad6
Greg Turnquist <gturnquist@pivotal.io>
2015-05-04 10:29:45 -0500

    Add extension point to decorate RestTemplate or other parts
    * Create a more extensible endpoint instead of initSubApis() found in many providers
    * Add documentation to better explain how to extend and augment existing Template solutions
    * Craft test cases providing backwards compatibility and show new extensibility

--- spring-social-core/src/main/java/org/springframework/social/oauth2/AbstractOAuth2ApiBinding.java
+++ spring-social-core/src/main/java/org/springframework/social/oauth2/AbstractOAuth2ApiBinding.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -37,12 +38,13 @@ import org.springframework.web.client.RestTemplate;
 /**
  * Base class for OAuth 2-based provider API bindings.
  * @author Craig Walls
+ * @author Greg Turnquist
  */
-public abstract class AbstractOAuth2ApiBinding implements ApiBinding {
+public abstract class AbstractOAuth2ApiBinding implements ApiBinding, InitializingBean {
 
 	private final String accessToken;
 
-	private final RestTemplate restTemplate;
+	private RestTemplate restTemplate;
 
 	/**
 	 * Constructs the API template without user authorization. This is useful for accessing operations on a provider's API that do not require user authorization.
@@ -199,5 +201,36 @@ public abstract class AbstractOAuth2ApiBinding implements ApiBinding {
 		client.setRequestFactory(ClientHttpRequestFactorySelector.getRequestFactory());
 		return client;
 	}
+
+	/**
+	 * After construction, include option to decorate the {@link RestTemplate} followed by an optional
+	 * configuration step. Many providers initialize sub-APIs, and this provides a convenient hook.
+	 * @throws Exception
+	 */
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		this.restTemplate = postProcess(this.restTemplate);
+		postConstructionConfiguration();
+	}
+
+	/**
+	 * Extensible hook to decorate {@link RestTemplate} or wrap it with a proxy of any type. By default, it just passes it through with no changes.
+	 *
+	 * @param restTemplate
+	 * @return
+	 */
+	protected RestTemplate postProcess(RestTemplate restTemplate) {
+		return restTemplate;
+	}
+
+	/**
+	 * An extension point to perform key initialization after everything is configured. Existing providers
+	 * are encouraged to migrate any form of constructor-based initialization into this method.
+	 *
+	 * NOTE: To not break backwards compatibility, this method defaults to doing nothing.
+	 */
+	protected void postConstructionConfiguration() {
+	}
+
 
 }
